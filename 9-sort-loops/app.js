149df54068430386915d4ff3062a90d55585c6a7
vlntnb <belyaevavo@yandex.ru>
2023-11-28 12:34:10 +0300

    1

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,1 +1,13 @@
-
+let arr =[1, 40, -5, 10, 0];
+function sort(){
+   for(let i = 0; i < arr.length; i++){
+for(let j = i+1; j < arr.length; j++){
+    if (arr[i] > arr[j]){
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+   }
+   return arr;
+}   
+sort();
+console.log(arr);
